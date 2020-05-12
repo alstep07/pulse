753d1c19a86d363ee89c5a137fdb51f1666836b3
Sasha <alexstep07@icloud.com>
2020-05-12 23:50:20 +0300

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 const slider = tns({
+	speed: 1200,
 	container: ".carousel__inner",
 	items: 1,
 	slideBy: "page",
@@ -20,3 +21,32 @@ document.querySelector(".prev").addEventListener("click", function () {
 document.querySelector(".next").addEventListener("click", function () {
 	slider.goTo("next");
 });
+
+$("ul.catalog__tabs").on("click", "li:not(.catalog__tab_active)", function () {
+	$(this)
+		.addClass("catalog__tab_active")
+		.siblings()
+		.removeClass("catalog__tab_active")
+		.closest("div.container")
+		.find("div.catalog__content")
+		.removeClass("catalog__content_active")
+		.eq($(this).index())
+		.addClass("catalog__content_active");
+});
+
+function toggleSlide(item) {
+	$(item).each(function (i) {
+		$(this).on("click", function (e) {
+			e.preventDefault();
+			$(".catalog-item__content")
+				.eq(i)
+				.toggleClass("catalog-item__content_active");
+			$(".catalog-item__list")
+				.eq(i)
+				.toggleClass("catalog-item__list_active");
+		});
+	});
+}
+
+toggleSlide(".catalog-item__back");
+toggleSlide(".catalog-item__link");
